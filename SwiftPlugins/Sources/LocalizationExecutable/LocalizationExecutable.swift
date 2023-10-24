8ebe8bce754e6ac24f5d80920c3c5b895ca4fe79
Kosta Nedeljkovic <kosta.nedeljkovic@gmail.com>
2023-10-24 15:50:15 +0200

    Create sources directory with LocalizationExecutable & VerifyTranslations

---
+++ SwiftPlugins/Sources/LocalizationExecutable/LocalizationExecutable.swift
@@ -0,0 +1,84 @@
+import Foundation
+
+@main
+class LocalizationExecutable {
+
+  static func main() {
+      Executor.executeShell(.pullPhrase)
+
+      Executor.verifyTranslations()
+      
+      Executor.executeShell(.generateLocalization)
+  }
+}
+
+class Executor {
+
+    enum Command {
+        case pullPhrase, generateLocalization
+
+        var cmd: String {
+            switch self {
+            case .pullPhrase:
+                return "/opt/homebrew/bin/phrase pull"
+            case .generateLocalization:
+                return "/opt/homebrew/bin/swiftgen config run --verbose"
+            }
+        }
+
+        var config: String {
+            switch self {
+            case .pullPhrase:
+                return ".phrase.yml"
+            case .generateLocalization:
+                return "SwiftGen/swiftgen-localization.yml"
+            }
+        }
+    }
+
+    class func verifyTranslations() {
+        let verificator = TranslationsVerificator()
+
+        let shouldGenerateReportFile = CommandLine.arguments.contains(generateReportFileArgumentName)
+        verificator.verifyTranslations(shouldGenerateReportFile: shouldGenerateReportFile)
+    }
+
+    class func executeShell(_ command: Command) {
+        print(Self.shell(command))
+    }
+
+    @discardableResult
+    private class func shell(_ command: Command) -> String {
+// This check is needed bacause Process is only available on macOS
+#if os(macOS)
+        let process = Process()
+        let pipe = Pipe()
+
+        process.standardOutput = pipe
+        process.standardError = pipe
+
+        let configPath = "\(process.currentDirectoryPath)/../\(command.config)"
+        process.arguments = ["-c", "\(command.cmd) --config \(configPath)"]
+
+        process.launchPath = "/bin/zsh"
+        process.standardInput = nil
+
+        var environment = ProcessInfo.processInfo.environment
+        environment["PROJECT_DIR"] = process.currentDirectoryPath
+        process.environment = environment
+
+        do {
+            try process.run()
+            process.waitUntilExit()
+        } catch {
+            print("Failed to run the process: \(error)")
+        }
+
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(data: data, encoding: .utf8)!
+
+        return output
+#endif
+        return "Not supported. Executable only on macOS."
+    }
+}
